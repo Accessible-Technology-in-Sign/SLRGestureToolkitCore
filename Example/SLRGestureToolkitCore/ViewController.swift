a55673962dfb5a6b78f024ccb46ed7d7c4c65579
Wavin Dev <eshwavin@gmail.com>
2024-11-05 07:33:23 -0500

    Added basic example

--- Example/SLRGestureToolkitCore/ViewController.swift
+++ Example/SLRGestureToolkitCore/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 import SLRGestureToolkitCore
 
 class ViewController: UIViewController {
+    
+    private let basicExampleButton: UIButton = {
+        let button = UIButton()
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.cornerStyle = .medium
+        buttonConfiguration.title = String(localized: "Basic Example")
+        buttonConfiguration.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 24, bottom: 12, trailing: 24)
+        button.configuration = buttonConfiguration
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(basicExampleButton)
+        
+        basicExampleButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints: [NSLayoutConstraint] = [
+            basicExampleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            basicExampleButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        basicExampleButton.addTarget(self, action: #selector(didTapShowBasicExample(_:)), for: .touchUpInside)
+    }
+    
+    @objc private func didTapShowBasicExample(_ sender: UIButton) {
+        let basicExampleViewController = BasicExampleViewController()
+        basicExampleViewController.modalPresentationStyle = .fullScreen
+        present(basicExampleViewController, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
